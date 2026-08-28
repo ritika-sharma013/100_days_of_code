df7930ab0985de5113423f45153dae7d371000aa
Rwitzeeka <ritikasharmadelhi2005@gmail.com>
2026-08-29 03:44:33 +0530

    working with strings

--- Python-for-ai/main.py
+++ Python-for-ai/main.py
@@ -1,5 +1,11 @@
-import requests
+print("Hello world !")
+first_name = "Ritika"
+last_name = "Sharma"
+full_name = first_name + " " + last_name
+print(full_name)
 
-# Download a web page
-response = requests.get("https://api.github.com")
-print(response.status_code)  # Should print 200 
+# Working with string methods 
+text = "Hello PYTHON"
+print(text.lower()) # converts the string to lowercase
+print(text.upper()) # converts the string to uppercase
+
